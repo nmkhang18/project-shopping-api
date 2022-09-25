b35390f9b1d435ccfda02075b0b19b71edb1ab71
Khang <minhkhangg18@gmail.com>
2022-09-25 17:01:28 +0700

    second-commit

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,11 +1,14 @@
 // get the client
+const { provider } = require('@hapi/joi/lib/cache');
 const mysql = require('mysql2/promise')
+require('dotenv').config()
+
 
 console.log("Creating connection pool...");
 
 module.exports.pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'shopping',
-    // password: 'password'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 })
